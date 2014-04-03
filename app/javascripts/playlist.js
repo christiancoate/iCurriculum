3add0fe26fd5ae506386f7a352f8016e8c12b057
Christian Coate <christiancoate@gmail.com>
2014-04-03 11:18:23 -0700

    t

--- app/javascripts/playlist.js
+++ app/javascripts/playlist.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 $('video').draggable();
 $('#playlist').droppable(function(){
-  var $playlist = $('video').val();
+  var $playlist = $('#playlist').val();
   var playlistScreen = $('<h4><a href="playlist.html">View Your Playlist</a></h4>')
+  $('#yourPlaylist').append($playlist);
 });
 });
