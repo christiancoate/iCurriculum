625083eeedd57e15d77e7ccc9f811b4d55f488fc
Christian Coate <christiancoate@gmail.com>
2014-04-03 11:20:33 -0700

    Update playlist.js
    Just needed to make one more commit.

--- app/javascripts/playlist.js
+++ app/javascripts/playlist.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $('video').draggable();
 $('#playlist').droppable(function(){
   var $playlist = $('#playlist').val();
-  var playlistScreen = $('<h4><a href="playlist.html">View Your Playlist</a></h4>')
+  // I did not exactly need that fith line of code.
   $('#yourPlaylist').append($playlist);
 });
 });
