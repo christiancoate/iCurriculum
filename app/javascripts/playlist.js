45f70e9f87cfdb2bd7127692c9265bbdafe44923
Christian Coate <christiancoate@gmail.com>
2014-04-03 13:58:48 -0700

    Update playlist.js
    I used the wrong method.

--- app/javascripts/playlist.js
+++ app/javascripts/playlist.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $('video').draggable();
 $('#playlist').droppable(function(){
-  var $playlist = $('#playlist').val();
+  var $playlist = $('#playlist').html();
   // I do not exactly need that fith line of code.
   $('#yourPlaylist').append($playlist);
 });
