39e7c0fc5a3c14512c451b35ac50ba9ba4c22d29
Christian Coate <christiancoate@gmail.com>
2014-04-03 11:22:15 -0700

    Update playlist.js
    This one is not that important. Last one...

--- app/javascripts/playlist.js
+++ app/javascripts/playlist.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $('video').draggable();
 $('#playlist').droppable(function(){
   var $playlist = $('#playlist').val();
-  // I did not exactly need that fith line of code.
+  // I do not exactly need that fith line of code.
   $('#yourPlaylist').append($playlist);
 });
 });
